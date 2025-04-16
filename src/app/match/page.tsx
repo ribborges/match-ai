c293db489eb061f2d26f92dee2565053b174ae5c
Richard Borges <ribborges@outlook.com>
2025-04-16 20:02:51 -0300

    feat: add TagsInput component and refactor form layout in Match page

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -1,6 +1,8 @@
 import { Stars } from "react-bootstrap-icons";
 
-import { Input, LocationInput } from "@/components/Input";
+import { Input, LocationInput, TagsInput } from "@/components/Input";
+import Button from "@/components/Button";
+import { areasOfInterest } from "@/data";
 
 export default function Match() {
     return (
@@ -12,9 +14,25 @@ export default function Match() {
             <h3 className="text-lg text-zinc-600 dark:text-zinc-400">
                 Preencha seus dados para encontrarmos as melhores conexões para você
             </h3>
-            <form className="p-2 flex flex-col gap-4">
-                <Input label="Nome" placeholder="Digite seu nome completo" />
-                <LocationInput label="Localização" placeholder="Digite sua localização" />
+            <form className="p-2 flex-1 flex flex-col">
+                <div className="flex-1 flex flex-col gap-4">
+                    <Input
+                        label="Nome"
+                        placeholder="Digite seu nome completo"
+                    />
+                    <TagsInput
+                        label="Área de interesse"
+                        placeholder="Digite uma área de interesse"
+                        data={areasOfInterest}
+                    />
+                    <LocationInput
+                        label="Localização"
+                        placeholder="Digite sua localização"
+                    />
+                </div>
+                <Button type="submit">
+                    Buscar conexões
+                </Button>
             </form>
         </div>
     );
