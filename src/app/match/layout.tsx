1484fff32e07c605e222bd4189565283de6f8476
Richard <46366493+ribborges@users.noreply.github.com>
2025-04-16 02:49:00 -0300

    style: update main layout styles

--- src/app/match/layout.tsx
+++ src/app/match/layout.tsx
@@ -11,14 +11,14 @@ export default function MatchLayout({
         <div className="flex flex-col min-h-screen">
             <AnimatedBackground topLayerClassName="backdrop-blur-3xl" />
             <Header />
-            <main className="flex-1 flex flex-col items-strech justify-center gap-6 p-4 min-h-screen">
+            <main className="flex-1 flex flex-col items-strech justify-center gap-6 md:p-4 min-h-screen">
                 <div
                     className="
                         flex-1
                         mt-26 md:m-18 lg:m-28 p-4
                         flex
                         bg-zinc-100 dark:bg-zinc-950
-                        md:border border-zinc-200 dark:border-zinc-900 md:rounded-2xl
+                        border-t border-b md:border border-zinc-200 dark:border-zinc-900 md:rounded-2xl
                     "
                 >
                     {children}
