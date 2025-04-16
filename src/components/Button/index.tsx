5399bedecb2303488fda31aae14cc059279d7877
Richard Borges <ribborges@outlook.com>
2025-04-16 00:00:10 -0300

    feat: add Button component with props and styles

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,30 @@
+interface ButtonProps {
+    id?: string;
+    name?: string;
+    onClick?: () => void;
+    disabled?: boolean;
+    type?: "button" | "submit" | "reset";
+    children: React.ReactNode;
+}
+
+export default function Button(props: ButtonProps) {
+    return (
+        <button 
+            id={props.id}
+            name={props.name}
+            onClick={props.onClick}
+            disabled={props.disabled}
+            type={props.type}
+            className="
+                flex
+                bg-gradient-to-tr from-indigo-500 to-yellow-500 hover:from-yellow-500 hover:to-indigo-500
+                text-white font-bold p-4 rounded-full
+                shadow-lg hover:shadow-black/40 dark:hover:shadow-zinc-100/20
+                hover:scale-105
+                transition duration-300 ease-in-out
+            "
+        >
+            {props.children}
+        </button>
+    );
+}
