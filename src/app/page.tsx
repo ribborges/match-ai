d8823de1a638f65bbbe27c93ee6baca1ceecb9c1
Richard Borges <ribborges@outlook.com>
2025-04-17 01:32:25 -0300

    feat: add footer component and logo to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 import AnimatedBackground from "@/components/AnimatedBackground";
 import AnimatedButton from "@/components/AnimatedButton";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   const router = useRouter();
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <AnimatedBackground />
-      <header className="flex items-center justify-center gap-4 p-8 bg-gradient-to-b from-black to-black/0">
+      <header className="fixed top-0 left-0 right-0 flex items-center justify-center gap-4 p-8 bg-gradient-to-b from-black to-black/0">
         <Image
           src="/logoLight.svg"
           alt="Match.AI Logo"
@@ -29,11 +30,12 @@ export default function Home() {
         />
         <h1 className="text-4xl font-black">match.ai</h1>
       </header>
-      <main className="flex-1 flex flex-col items-center justify-center gap-6 p-4">
+      <main className="flex-1 flex flex-col items-center justify-center gap-6 p-4 min-h-screen">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-black">Encontre pessoas como você</h1>
         <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">Matchmaking feito por inteligência artifical</h2>
         <AnimatedButton onClick={() => router.push("match")}>Comece agora</AnimatedButton>
       </main>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,24 @@
+import Image from "next/image";
+
 export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
         <footer
             className="
-                flex items-center justify-center gap-4
+                flex flex-col items-center justify-center gap-4
                 p-12
                 bg-zinc-100 dark:bg-zinc-950
                 border-t border-zinc-200 dark:border-zinc-900
             "
         >
-            match.ai &copy; | {currentYear}
+            <Image
+                src="/logoLegal.svg"
+                alt="Match.AI Logo"
+                width={64}
+                height={64}
+            />
+            <span>match.ai &copy; | {currentYear}</span>
         </footer>
     );
 }
